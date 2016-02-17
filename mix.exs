641aef0fcdc04b3df7de5d165193577c9af592bf
Michael Shapiro <koudelka@ryoukai.org>
2016-02-16 16:56:25 -0800

    Version 0.0.3

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule LzString.Mixfile do
 
   def project do
     [app: :lz_string,
-     version: "0.0.2",
+     version: "0.0.3",
      elixir: "~> 1.2",
      package: package,
      deps: deps(Mix.env),
